362f77a3df535196a3bc806522c7ef6f47198672
KasraRahimi <kasra.rahimi@polymtl.ca>
2023-11-22 15:34:40 -0500

    added useful youtube functions

---
+++ src/functions/youtube.ts
@@ -0,0 +1,19 @@
+import { Video } from "@src/classes/Video/Video";
+import { youtube } from "scrape-youtube";
+
+export const search = async (query: string): Promise<Video[]> => {
+    const results = await youtube.search(query);
+    const videos: Video[] = results.videos.map(video => new Video(video.title, video.link, video.duration));
+    if (videos.length <= 5) return Promise.resolve(videos);
+    return Promise.resolve(videos.splice(0, 5));
+};
+
+export const videoToString = (video: Video): string => {
+    return `${video.title} (${video.durationString})`;
+};
+
+export const videoArrToString = (videos: Video[]): string => {
+    return videos.map((video: Video, index: number) => {
+        return `${index + 1}. ${videoToString(video)}`;
+    }).join('\n');
+}
