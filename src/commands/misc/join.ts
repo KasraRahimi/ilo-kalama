0407ed7a015715e60963d9d0b3bb2153d12db21c
KasraRahimi <kasra.rahimi@polymtl.ca>
2023-11-22 08:45:46 -0500

    functional join command that plays an aot opening

--- src/commands/misc/join.ts
+++ src/commands/misc/join.ts
@@ -1,5 +1,9 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction, GuildMember } from "discord.js";
 import { VoiceConnectionStatus, AudioPlayerStatus, joinVoiceChannel, createAudioPlayer, createAudioResource } from "@discordjs/voice";
+import ytdl from 'ytdl-core';
+import { join } from 'path';
+
+const link = 'https://www.youtube.com/watch?v=jhYg5NrN-r8';
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -25,10 +29,24 @@ module.exports = {
             adapterCreator: adaptorCreator
         });
 
-        const player = createAudioPlayer();
-        const resource = createAudioResource('./../../sound/nyan-cat.mp3');
-        player.play(resource);
+        let player = createAudioPlayer();
+        player.on('error', (error) => {
+            console.error(error);
+        });
+        player.on(AudioPlayerStatus.Playing, () => {
+            console.log('playing audio');
+        });
+        player.on(AudioPlayerStatus.Idle, () => {
+            console.log('idle');
+        });
+        player.on(AudioPlayerStatus.Paused, () => {
+            console.log('paused');
+        });
         connection.subscribe(player);
+
+        const video = await ytdl(link, { filter: 'audioonly' });
+        const resource = createAudioResource(video);
+        player.play(resource);
         console.log('playing it');
         const thing = await new Promise((resolve) => {
             setTimeout(resolve, 180_000);
