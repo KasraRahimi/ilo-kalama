22eb578b8f61b5fc9221d86ae40508b0a3904808
KasraRahimi <kasra.rahimi@polymtl.ca>
2023-11-22 10:54:20 -0500

    added more methods for playing youtube video

--- src/classes/Session/Session.ts
+++ src/classes/Session/Session.ts
@@ -1,6 +1,6 @@
-import { AudioPlayer, VoiceConnection, createAudioPlayer, joinVoiceChannel } from '@discordjs/voice';
+import { AudioPlayer, AudioPlayerStatus, VoiceConnection, createAudioPlayer, createAudioResource, joinVoiceChannel } from '@discordjs/voice';
 import { Video } from '@src/classes/Video/Video';
-import { Channel, Guild, VoiceBasedChannel } from 'discord.js';
+import { Channel, Guild, TextChannel, VoiceBasedChannel } from 'discord.js';
 
 const TIMEOUT = 30_000;
 
@@ -9,15 +9,28 @@ export class Session {
     private _currentVideo: Video | null;
     private _connection: VoiceConnection;
     private _player: AudioPlayer;
-    _textChannel: Channel;
+    private _timeout: NodeJS.Timeout | undefined;
+    _textChannel: TextChannel;
 
-    constructor(guild: Guild, textChannel: Channel, vc: VoiceBasedChannel) {
+    constructor(guild: Guild, textChannel: TextChannel, vc: VoiceBasedChannel) {
         this._guild = guild;
         this._textChannel = textChannel;
         this._currentVideo = null;
 
         this._connection = this.getConnection(vc.id);
         this._player = createAudioPlayer();
+        this._connection.subscribe(this._player);
+        this.setPlayerEvents();
+    }
+
+    setVideo(video: Video) {
+        this._currentVideo = video;
+    }
+
+    async play() {
+        if (this._currentVideo === null) return;
+        const resource = createAudioResource(await this._currentVideo.getAudio());
+        this._player.play(resource);
     }
 
     private getConnection(channelId: string): VoiceConnection {
@@ -29,5 +42,23 @@ export class Session {
         });
     }
 
+    private setPlayerEvents() {
+        // Handle Idle state
+        this._player.on(AudioPlayerStatus.Idle, () => {
+            this._textChannel.send('Done playing audio. Leaving voice channel soon.');
+            this._timeout = setTimeout(() => {
+                this.handleSessionEnd();
+            }, TIMEOUT);
+        });
+        // Handle Playing state
+        this._player.on(AudioPlayerStatus.Playing, () => {
+            if (this._timeout) return;
+            clearTimeout(this._timeout);
+        });
+    }
 
+    private handleSessionEnd() {
+        this._player.stop();
+        this._connection.destroy();    
+    }
 }
