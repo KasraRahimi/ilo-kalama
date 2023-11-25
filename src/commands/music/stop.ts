00371848b8f8a7c5c65f02c4f73bb3c731ee6c31
Kasra <kasra.rahimi@polymtl.ca>
2023-11-25 17:19:23 -0500

    made new stop command

---
+++ src/commands/music/stop.ts
@@ -0,0 +1,22 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import { Ilo } from "@src/Ilo";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('stop')
+        .setDescription('clears the queue, stops the music and leaves the channel.'),
+    needsClient: true,
+    async execute(interaction: ChatInputCommandInteraction, client: Ilo) {
+        if (!interaction.inGuild()) {
+            await interaction.reply('you can only use this command in a server');
+            return;
+        }
+        const session = client.sessions.get(interaction.guildId);
+        if (!session) {
+            await interaction.reply('I\'m not playing anything in this server');
+            return;
+        }
+        await interaction.reply('Stopping the music and clearing the queue.');
+        await session.handleSessionEnd();
+    }
+}
